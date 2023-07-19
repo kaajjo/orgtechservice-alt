5384bf3e67d66968bac2ae6c6c5efc0c2cca22b7
kaajjo <claymanoff@gmail.com>
2023-07-19 19:26:19 +0300

    fix: prevent repeating in pagination * If the requested page does not exist, the API will return a first page. Add a `maxPageNumber` to know when to stop requesting new pages

--- app/src/main/java/com/kaajjo/orgtechservice/data/remote/paging/traffic/TrafficPagingSource.kt
+++ app/src/main/java/com/kaajjo/orgtechservice/data/remote/paging/traffic/TrafficPagingSource.kt
@@ -9,6 +9,9 @@ class TrafficPagingSource(
     private val trafficService: TrafficService,
     private val key: String
 ) : PagingSource<Int, Traffic>() {
+    // Max page number. Needed for pagination to know when to stop.
+    // Because if requested page does not exist the API will return first page.
+    private var totalPages = 1
     override fun getRefreshKey(state: PagingState<Int, Traffic>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
@@ -19,12 +22,14 @@ class TrafficPagingSource(
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Traffic> {
         return try {
             val page = params.key ?: 1
+
             val response = trafficService.getTrafficMonthly(key = key, page = page)
+            totalPages = response.body()?.pagesCount ?: 1
 
             LoadResult.Page(
                 data = response.body()?.traffic ?: emptyList(),
                 prevKey = if (page == 1) null else page - 1,
-                nextKey = if (response.body()?.traffic == null || response.body()?.traffic!!.isEmpty()) null else page + 1
+                nextKey = if (response.body()?.traffic == null || response.body()?.page!! >= totalPages) null else page + 1
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
