f1cb2c445734db202f6cff4ca592a5c2479a21ca
kaajjo <claymanoff@gmail.com>
2024-03-18 00:16:17 +0300

    init `GlobalExceptionHandler`

--- app/src/main/java/com/kaajjo/orgtechservice/ui/MainActivity.kt
+++ app/src/main/java/com/kaajjo/orgtechservice/ui/MainActivity.kt
@@ -7,28 +7,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
+import com.kaajjo.orgtechservice.core.utils.GlobalExceptionHandler
 import com.kaajjo.orgtechservice.ui.screen.NavGraphs
+import com.kaajjo.orgtechservice.ui.screen.crash.CrashActivity
 import com.kaajjo.orgtechservice.ui.theme.OrgtechserviceTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
-
-val LocalUserApiKey = compositionLocalOf<String> { "" }
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        if (!BuildConfig.DEBUG) {
-//            GlobalExceptionHandler.initialize(
-//                applicationContext = applicationContext,
-//                activityToBeLaunched = CrashActivity::class.java
-//            )
-//        }
+        GlobalExceptionHandler.initialize(
+            applicationContext = applicationContext,
+            activityToBeLaunched = CrashActivity::class.java
+        )
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
